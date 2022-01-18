32c1af9ceb5502adc4dea75d25bd60863aed70c0
ILUVpwny <efmcid@gmail.com>
2022-01-18 08:35:45 +0700

    Add more projects.

--- src/components/ProjectList/ProjectList.js
+++ src/components/ProjectList/ProjectList.js
@@ -21,10 +21,20 @@ export default class ProjectList extends Component {
                         desc='An API for minecraft Mod based on fabric-loader. The goal is to unify all mod entity management system under one group of interface to encourage 
                         inter-project compatibiity. The logic behind the mod is base on ESC. With game objects such as ItemStack, Entity, BlockEntity carry components callled "Abilities" which will be the part that actually do work.'
                         githubLink='https://github.com/ILUVpwny/PwnyAPI'/>
-                    <Project />
-                    <Project />
-                    <Project />
-                    <Project />
+                    <Project
+                        name='PwnyPipe'
+                        desc='An implementation of PwnyAPI Abilities System. Allowing interaction(transfer) between different mods that implement default PwnyAPI Abilities interface, without them directly
+                        interact with each other'
+                        githubLink='https://github.com/ILUVpwny/PwnyPipe'/>
+                    <Project
+                        name='GraderBot'
+                        desc='A JAVA web scraping program. Scraping data from Walailak grader system. Allowing user to get more in-dept statistic of their score. Communicating with user
+                        through Discord.'
+                        githubLink='https://github.com/ILUVpwny/GraderBot'/>
+                    <Project
+                        name='PortfolioReact'
+                        desc="Basically this website. Created with React.JS library. It's  mostly mobile compatible."
+                        githubLink='https://github.com/ILUVpwny/PortfolioReact'/>
                 </div>
             </div>
         )
